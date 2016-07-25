ac92c8abfdf868bf250af8baf58611f0bf1f93b2
mzl <you@example.com>
2016-07-25 18:19:24 +0800

    初始化项目

---
+++ myXMPP/myXMPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myXMPP
+//
+//  Created by winbei on 16/7/25.
+//  Copyright © 2016年 winbei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
